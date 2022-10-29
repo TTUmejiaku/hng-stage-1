ce9ea8277bc8bfef28ea8889216fb2dd3cd4b95c
TTUmejiaku <templeumejiaku@gmail.com>
2022-10-30 00:42:12 +0100

    added footer component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,20 @@
 import LinksSection from "../components/LinksSection";
 import ProfileSection from "../components/ProfileSection";
+import SlackIcon from "../assets/slack.svg";
+import GitHub from "../assets/github.svg";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <div>
-      <div className="container mt-16">
+      <div className="container">
         <ProfileSection />
         <LinksSection />
+        <div className="flex items-center justify-center space-x-4 mt-10">
+          <img src={SlackIcon} alt="" className="cursor-pointer" />
+          <img src={GitHub} alt="" className="cursor-pointer" />
+        </div>
+        <Footer />
       </div>
     </div>
   );
