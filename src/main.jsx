f5bd5e43bb41f77da8b937fafb6f2f994293b5d2
TTUmejiaku <templeumejiaku@gmail.com>
2022-11-03 19:39:57 +0100

    chore: Removed comments

--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    {/* <App /> */}
   </React.StrictMode>
 );
